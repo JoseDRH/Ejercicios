d62ce6167560a5028735ff7e6c7469fc3998ee52
Jose del Ŕio <jose.delrio@bosonit.com>
2022-10-19 15:07:19 +0200

    commit block5

---
+++ block5-command-line/src/main/java/com/example/block5commandline/Block5CommandLineApplication.java
@@ -0,0 +1,39 @@
+package com.example.block5commandline;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class Block5CommandLineApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Block5CommandLineApplication.class, args);
+	}
+	@PostConstruct
+	public void hola(){
+		System.out.println("Hola desde clase inicial");
+	}
+	@Bean
+	CommandLineRunner hola2(){
+		return p-> System.out.println("Hola desde clase secundaria");
+	}
+	@Bean
+	CommandLineRunner hola3(@RequestParam String[] args){
+		return p->{
+			for (String a:
+				 args) {
+				System.out.println(a);
+			}
+		};
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
+}
