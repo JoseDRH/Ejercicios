3fac527319811a5a7c663f6d03a5a846ba2f31f8
Jose del Ŕio <jose.delrio@bosonit.com>
2022-11-28 18:12:41 +0100

    commit block12

--- block12-kafka/src/main/java/com/example/block12kafka/Block12KafkaApplication.java
+++ block12-kafka/src/main/java/com/example/block12kafka/Block12KafkaApplication.java
@@ -10,4 +10,8 @@ public class Block12KafkaApplication {
 		SpringApplication.run(Block12KafkaApplication.class, args);
 	}
 
+	//Para que el proyecyo funcione debe ejecutar el siguiente comando en el cmd
+	//docker run --name KafkaBroker -p 2181:2181 -p 3030:3030 -p 8081-8083:8081-8083 -p 9581-9585:9581-9585 -p 9092:9092 -e ADV_HOST=localhost landoop/fast-data-dev:latest
+
+
 }
